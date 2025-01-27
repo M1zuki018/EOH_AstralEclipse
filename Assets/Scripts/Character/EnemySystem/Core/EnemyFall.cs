7368a1a3cea84724dc7c96667083e73ccaf4419f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-01-27 11:35:23 +0900

    敵の落下時の処理を作成

---
+++ Assets/Scripts/Character/EnemySystem/Core/EnemyFall.cs
@@ -0,0 +1,23 @@
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// エネミーがステージから落下したときの処理を書いたクラス
+/// </summary>
+public class EnemyFall : MonoBehaviour
+{
+    [SerializeField] private float _fallHeight = -10f;
+    private Transform _enemy;
+
+    private void Start()
+    {
+        _enemy = this.transform;
+        
+        Observable
+            .EveryUpdate()
+            .Where(_ => _enemy.position.y < _fallHeight)
+            .Take(1)
+            .Subscribe(_ => gameObject.SetActive(false))
+            .AddTo(this);
+    }
+}
