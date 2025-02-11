98d74b8ef5c5d8e650feb8c84e4b85cf8af504da
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-11 15:30:37 +0900

    キーアイテムを獲得したらバリアを解除する機能を実装

--- Assets/Scripts/Stage/Gimmick/KeyItem.cs
+++ Assets/Scripts/Stage/Gimmick/KeyItem.cs
@@ -12,6 +12,7 @@ public class KeyItem : InteractableItemBase
 {
     [SerializeField] private string _keyName;
     [SerializeField] private float _moveY = 1.5f;
+    [SerializeField] private BariierSystem _bariierSystem; //行動範囲を制限しているバリアを管理するクラス
     private Transform _keyObject; //親オブジェクトのトランスフォーム
     
     [SerializeField] private List<Health> _targets = new List<Health>(); //倒さなければいけないエネミーのHealthクラスを管理する
@@ -24,6 +25,7 @@ public class KeyItem : InteractableItemBase
     
     public override async void Interact()
     {
+        //まだ範囲内に敵が残っていた場合
         foreach (IHealth target in _targets)
         {
             if (!target.IsDead)
@@ -40,6 +42,8 @@ public class KeyItem : InteractableItemBase
         //CameraManager.Instance.UseTargetGroup(_keyObject, 1, 0.5f);
         //CameraManager.Instance.UseCamera(1);
         
+        //敵が残っておらず、キーアイテムが取得可能な状態の時
+        
         AudioManager.Instance.PlaySE(6);
         UIManager.Instance.QuestUpdate();
         
@@ -48,6 +52,7 @@ public class KeyItem : InteractableItemBase
         {
             inventory.AddKey(_keyName);
             Destroy(gameObject.transform.parent.gameObject); //キーを追加したらオブジェクトを削除する
+            _bariierSystem.HideBariier();
         }
         
         //CameraManager.Instance.UseCamera(0);
