57b6b851a03d58b3561e259264923cb9752102d6
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-11 15:13:59 +0900

    行動範囲制限のための仕組みを実装

---
+++ Assets/Scripts/Stage/Gimmick/BariierSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BarrierTriggerとBarrierオブジェクトを管理するシステム
+/// </summary>
+public class BariierSystem : MonoBehaviour
+{
+    [SerializeField] List<BariierTrigger> _bariierTriggers = new List<BariierTrigger>();
+    [SerializeField] GameObject _bariierA;
+    [SerializeField] GameObject _bariierB;
+    
+    private void Start()
+    {
+        foreach (var trigger in _bariierTriggers)
+        {
+            trigger.Initialize(this);
+        }
+    }
+
+    /// <summary>
+    /// バリアの位置を変更する
+    /// </summary>
+    public void SetBariierPos(Vector3 posA, Vector3 posB)
+    {
+        _bariierA.transform.position = posA;
+        _bariierB.transform.position = posB;
+        _bariierA.SetActive(true);
+        _bariierB.SetActive(true);
+    }
+
+    /// <summary>
+    /// バリアオブジェクトを非アクティブにする
+    /// </summary>
+    public void HideBariier()
+    {
+        _bariierA.SetActive(false);
+        _bariierB.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Stage/Gimmick/BariierTrigger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// Barrierを移動させる時に使用するTrigger
+/// </summary>
+public class BariierTrigger : MonoBehaviour
+{
+    [SerializeField] private Vector3 _bariierPosA;
+    [SerializeField] private Vector3 _bariierPosB;
+    private BariierSystem _bariierSystem;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void Initialize(BariierSystem bariierSystem)
+    {
+        _bariierSystem = bariierSystem;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _bariierSystem.SetBariierPos(_bariierPosA, _bariierPosB); //バリアの位置を変更する
+            enabled = false; //以降このスクリプトは使用しない
+        }
+    }
+}
