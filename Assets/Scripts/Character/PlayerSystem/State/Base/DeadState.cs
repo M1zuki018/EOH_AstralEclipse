808d0c2bf8cf70a79834e6210564a4a829fdab17
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-18 23:14:52 +0900

    各ステートのクラスを仮実装

---
+++ Assets/Scripts/Character/PlayerSystem/State/Base/DeadState.cs
@@ -0,0 +1,49 @@
+using Cysharp.Threading.Tasks;
+
+
+namespace PlayerSystem.State.Base
+{
+    /// <summary>
+    /// 死亡状態
+    /// </summary>
+    public class DeadState : IState
+    {
+        
+        private readonly StateMachineManager _smm;
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public DeadState(StateMachineManager smm)
+        {
+            _smm = smm;
+        }
+
+        public async UniTask Enter()
+        {
+            // Idle状態に入るときの処理（例えばアニメーションの開始など）
+            _smm.SetIdleAnimation();
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask Execute()
+        {
+            // 毎フレーム呼ばれる処理（状態遷移など）
+            if (_smm.IsRunning)
+            {
+                //_smm.TransitionToState(new RunningState(_smm));  // RunningStateへの遷移
+            }
+            else if (_smm.IsAttacking)
+            {
+                //_smm.TransitionToState(new AttackState(_smm));  // AttackStateへの遷移
+            }
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask Exit()
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Character/PlayerSystem/State/Base/RunState.cs
@@ -0,0 +1,48 @@
+using Cysharp.Threading.Tasks;
+
+namespace PlayerSystem.State.Base
+{
+    /// <summary>
+    /// 走り状態
+    /// </summary>
+    public class RunState : IState
+    {
+        
+        private readonly StateMachineManager _smm;
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public RunState(StateMachineManager smm)
+        {
+            _smm = smm;
+        }
+
+        public async UniTask Enter()
+        {
+            // Idle状態に入るときの処理（例えばアニメーションの開始など）
+            _smm.SetIdleAnimation();
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask Execute()
+        {
+            // 毎フレーム呼ばれる処理（状態遷移など）
+            if (_smm.IsRunning)
+            {
+                //_smm.TransitionToState(new RunningState(_smm));  // RunningStateへの遷移
+            }
+            else if (_smm.IsAttacking)
+            {
+                //_smm.TransitionToState(new AttackState(_smm));  // AttackStateへの遷移
+            }
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask Exit()
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+
+}
