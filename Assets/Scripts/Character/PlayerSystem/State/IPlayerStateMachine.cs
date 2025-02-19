2e4fb17cac11220b546b41db28da91380407bbb2
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-19 22:16:13 +0900

    サマリー修正

--- Assets/Scripts/Character/PlayerSystem/State/IPlayerStateMachine.cs
+++ Assets/Scripts/Character/PlayerSystem/State/IPlayerStateMachine.cs
@@ -2,7 +2,7 @@ using PlayerSystem.Input;
 using PlayerSystem.State;
 
 /// <summary>
-/// ベースステートマシンにプレイヤーの要素を追加するためのインターフェース
+/// ベースステートにプレイヤーの要素を追加するためのインターフェース
 /// </summary>
 public interface IPlayerStateMachine
 {
