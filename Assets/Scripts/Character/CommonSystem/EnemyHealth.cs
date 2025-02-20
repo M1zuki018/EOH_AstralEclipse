ba6e7b3a8b5930527a7422decbdd718c80be4397
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-21 00:39:54 +0900

    EnemyHealthクラスを別に作成

---
+++ Assets/Scripts/Character/CommonSystem/EnemyHealth.cs
@@ -0,0 +1,59 @@
+using System;
+using PlayerSystem.Fight;
+using UnityEngine;
+
+/// <summary>
+/// 敵のHPを管理するクラス
+/// </summary>
+public class EnemyHealth : MonoBehaviour, IHealth
+{
+    [SerializeField] private int _maxHPData = 100;
+    public int MaxHP { get; private set; } = 100; //最大HP
+    public int CurrentHP { get; private set; } //現在のHP
+    public bool IsDead => CurrentHP <= 0; //HPが0以下になったら死亡する
+    public event Action<int, GameObject> OnDamaged; //ダメージを受けた時のイベント
+    public event Action<int, GameObject> OnHealed; //回復イベント
+    public event Action<GameObject> OnDeath; //死亡イベント
+
+    
+    private void Awake()
+    {
+        MaxHP = _maxHPData;
+        CurrentHP = _maxHPData; //HPを初期化する
+    }
+    
+    /// <summary>
+    /// ダメージを受ける処理の実装
+    /// </summary>
+    public void TakeDamage(int amount, GameObject attacker)
+    {
+        if(IsDead) return; //死亡状態ならこれ以降の処理は行わない
+        
+        if(attacker.tag == this.tag) return;
+
+        if (this.gameObject.CompareTag("Player"))
+        {
+            TryGetComponent(out PlayerBrain brain);
+            if(brain.BB.IsSteping) return; //ステップ中の場合、ダメージを受けない
+        }
+        
+        CurrentHP -= amount;
+        OnDamaged?.Invoke(amount, attacker); //ダメージイベント発火
+
+        if (IsDead) //死亡判定
+        {
+            OnDeath?.Invoke(attacker); //死亡イベント発火
+        }
+    }
+
+    /// <summary>
+    /// 回復処理
+    /// </summary>
+    public void Heal(int amount, GameObject healer)
+    {
+        if(IsDead) return; //死亡状態ならこれ以降の処理は行わない
+        
+        CurrentHP += amount;
+        OnHealed?.Invoke(amount, healer);
+    }
+}
