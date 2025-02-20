b53843c1d4f087f6550f6d7e1a91f42b98ffdbaf
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-20 10:45:03 +0900

    MovementからJump処理を削除

--- Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerMovement.cs
@@ -19,7 +19,6 @@ namespace PlayerSystem.Movement
         private TrailRenderer _trailRenderer;
         
         private readonly float _rotationSpeed = 10f;
-        private readonly float _jumpMoveSpeed = 2f; //ジャンプ中の移動速度
 
         public PlayerMovement(
             PlayerBlackBoard bb, CharacterController characterController, Animator animator,
@@ -48,47 +47,25 @@ namespace PlayerSystem.Movement
             if (_bb.MoveDirection.sqrMagnitude > 0.01f)　//入力がある場合のみ処理を行う
             {
                 _trailRenderer.emitting = true; //軌跡をつける
-                
+
                 // カメラ基準で移動方向を計算
                 Vector3 cameraForward = Vector3.ProjectOnPlane(_playerCamera.transform.forward, Vector3.up).normalized;
                 Vector3 cameraRight = Vector3.ProjectOnPlane(_playerCamera.transform.right, Vector3.up).normalized;
-                Vector3 moveDirection = cameraForward *_bb.MoveDirection.z + cameraRight * _bb.MoveDirection.x;
-                
+                Vector3 moveDirection = cameraForward * _bb.MoveDirection.z + cameraRight * _bb.MoveDirection.x;
+
                 _bb.CorrectedDirection = moveDirection;
                 _moveNormal = moveDirection.normalized;
-                
+
                 // 回転をカメラの向きに合わせる
                 Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
                 _characterController.transform.rotation = Quaternion.Slerp(
                     _characterController.transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
 
-                if (_animator.applyRootMotion)
-                {
-                    // Animatorの速度を設定
-                    _animator.SetFloat("Speed", _moveNormal.sqrMagnitude * _bb.MoveSpeed, 0.1f, Time.deltaTime);
-                }
-                else
-                {
-                    //ルートモーションがオンじゃないとき＝ジャンプ中は、CharacterControllerのMoveメソッドを使用する
-                    
-                    float velocityY = _bb.Velocity.y; //Y軸の速度を保存する
-                    //移動中の速度は入力方向×ジャンプ中のスピード×現在のスピード（歩き/走り）
-                    _bb.Velocity = new Vector3(_moveNormal.x * _jumpMoveSpeed * _bb.MoveSpeed,
-                        velocityY, _moveNormal.z * _jumpMoveSpeed * _bb.MoveSpeed);
-                    
-                    _characterController.Move(_bb.Velocity * Time.deltaTime);
-                }
+                // Animatorの速度を設定
+                _animator.SetFloat("Speed", _moveNormal.sqrMagnitude * _bb.MoveSpeed, 0.1f, Time.deltaTime);
             }
             else
             {
-                //ジャンプ中の処理
-                if (_bb.IsJumping)
-                {
-                    float velocityY = _bb.Velocity.y; //Y軸の速度を保存する
-                    _bb.Velocity = new Vector3(0, velocityY, 0);
-                    
-                    _characterController.Move(_bb.Velocity * Time.deltaTime);
-                }
                 //緩やかに減速する。2fの部分を変化させると、減速の強さを変更できる
                 _moveNormal = Vector3.Lerp(_moveNormal, Vector3.zero, 2f * Time.deltaTime);
                 float speed = _moveNormal.magnitude * _bb.MoveSpeed;
