d77485969012334abe8c6eea3478e408903d5b6b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-20 22:44:16 +0900

    ガードの遷移を作成

--- Assets/Scripts/Character/PlayerSystem/State/Base/GuardState.cs
+++ Assets/Scripts/Character/PlayerSystem/State/Base/GuardState.cs
@@ -1,4 +1,6 @@
+using System;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace PlayerSystem.State.Base
 {
@@ -9,11 +11,23 @@ namespace PlayerSystem.State.Base
     {
         public GuardState(IPlayerStateMachine stateMachine) : base(stateMachine) { }
 
+        private bool _guardCancel;
+        
+        private Action _onGuardCancel;
+        
         /// <summary>
         /// ステートに入るときの処理
         /// </summary>
         public override async UniTask Enter()
         {
+            Debug.Log("Guard State: Enter");
+         
+            _onGuardCancel = () => _guardCancel = true;
+            
+            InputProcessor.OnGuard += _onGuardCancel;
+
+            BlackBoard.IsGuarding = true;
+            
             await UniTask.Yield();
         }
 
@@ -24,6 +38,21 @@ namespace PlayerSystem.State.Base
         {
             while (StateMachine.CurrentState.Value == BaseStateEnum.Guard)
             {
+                if (_guardCancel) // ガードキャンセル
+                {
+                    // 移動入力がなければ Idle へ
+                    if (BlackBoard.MoveDirection.sqrMagnitude < 0.01f)
+                    {
+                        StateMachine.ChangeState(BaseStateEnum.Idle);
+                        return;
+                    }
+                    // 移動入力があれば Move へ
+                    else
+                    {
+                        StateMachine.ChangeState(BaseStateEnum.Move);
+                        return;
+                    }
+                }
                 await UniTask.Yield();
             }
         }
@@ -33,6 +62,14 @@ namespace PlayerSystem.State.Base
         /// </summary>
         public override async UniTask Exit()
         {
+            BlackBoard.IsGuarding = false;
+            
+            // 状態をリセット
+            _guardCancel = false;
+            
+            // イベントを解除
+            InputProcessor.OnGuard -= _onGuardCancel;
+            
             await UniTask.Yield();
         }
     }
