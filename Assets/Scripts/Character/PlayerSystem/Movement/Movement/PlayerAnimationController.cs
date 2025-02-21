467ddeb3845b9a68355445b77a02f6d4d3100e74
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-21 20:13:41 +0900

    AnimationとTrailControllerの実装

--- Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerAnimationController.cs
+++ Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerAnimationController.cs
@@ -13,6 +13,33 @@ namespace PlayerSystem.Movement
         {
             _animator = animator;
         }
+        
+        /// <summary>
+        /// 移動速度をアニメーションに反映
+        /// </summary>
+        public void SetMoveSpeed(float speed)
+        {
+            _animator.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+        }
+
+        /// <summary>
+        /// ジャンプアニメーションを開始
+        /// </summary>
+        public void PlayJumpAnimation()
+        {
+            _animator.SetTrigger("Jump");
+            _animator.SetBool("IsJumping", true);
+            _animator.applyRootMotion = false;
+        }
+
+        /// <summary>
+        /// ジャンプ終了時のアニメーション処理
+        /// </summary>
+        public void StopJumpAnimation()
+        {
+            _animator.SetBool("IsJumping", false);
+            _animator.applyRootMotion = true;
+        }
     }
 
 }
--- Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerTrailController.cs
+++ Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerTrailController.cs
@@ -1,21 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// プレイヤーの足跡を表現するトレイルを管理するクラス
-/// </summary>
-public class PlayerTrailController : MonoBehaviour
+namespace PlayerSystem.Movement
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// プレイヤーの足跡を表現するトレイルを管理するクラス
+    /// </summary>
+    public class PlayerTrailController
     {
-        
-    }
+        private TrailRenderer _trailRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public PlayerTrailController(TrailRenderer trailRenderer)
+        {
+            _trailRenderer = trailRenderer;
+        }
+
+        /// <summary>
+        /// 軌跡を表示する
+        /// </summary>
+        public void EnableTrail()
+        {
+            _trailRenderer.emitting = true;
+        }
+
+        /// <summary>
+        /// 軌跡を非表示にする
+        /// </summary>
+        public void DisableTrail()
+        {
+            _trailRenderer.emitting = false;
+        }
     }
 }
