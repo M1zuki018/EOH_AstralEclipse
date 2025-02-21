a859e30b8c60ad00a6233cbc577b0a646700a606
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-21 10:25:38 +0900

    スキルデータを、今回のゲームで使用する分だけ管理できればいいように編集

--- Assets/Scripts/Character/CommonSystem/Data/SkillData.cs
+++ Assets/Scripts/Character/CommonSystem/Data/SkillData.cs
@@ -8,13 +8,12 @@ using UnityEngine;
 public struct SkillData
 {
     public int ID; //ID
-    public string Name; //スキル名
+    public SkillType Name; // スキルの名前
     public string Descriptionl; //スキルの説明
     public Sprite Icon; // スキルアイコン
     public float AttackMultiplier; // 攻撃倍率
     public float Cooldown; // クールタイム（秒）
     public int ResourceCost; // 消費リソース量
-    public SkillType Type; // スキルの種類
     public float CastTime; // 発動時間
     public float Range; // スキルの効果範囲
     public GameObject EffectPrefab; // エフェクトのPrefab
--- Assets/Scripts/Character/CommonSystem/Data/SkillType.cs
+++ Assets/Scripts/Character/CommonSystem/Data/SkillType.cs
@@ -3,14 +3,12 @@
 /// </summary>
 public enum SkillType
 {
-    /// <summary>近接攻撃</summary>
-    Melee,
-    /// <summary>遠距離攻撃</summary>
-    Ranged,
-    /// <summary>バフ</summary>
-    Buff,
-    /// <summary>デバフ</summary>
-    Debuff,
-    /// <summary>回復</summary>
-    Healing
+    /// <summary>スキル1</summary>
+    NebulaFrost,
+    /// <summary>スキル2</summary>
+    VolcanicBurst,
+    /// <summary>スキル3</summary>
+    AbyssBlaser,
+    /// <summary>スキル4</summary>
+    CelestialCreed,
 }
