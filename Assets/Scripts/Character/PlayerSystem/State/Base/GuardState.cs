1d4a1de3eb8c4966c78a26e6fd2f5aac8a3ecadd
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-02-28 16:22:24 +0900

    パリィ受付開始/受付終了を正確に操作するように

--- Assets/Scripts/Character/PlayerSystem/State/Base/GuardState.cs
+++ Assets/Scripts/Character/PlayerSystem/State/Base/GuardState.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 
 namespace PlayerSystem.State.Base
@@ -10,17 +11,30 @@ namespace PlayerSystem.State.Base
     {
         public GuardState(IPlayerStateMachine stateMachine) : base(stateMachine) { }
 
-        private int _count; // パリィ判定用のカウンター
+        private ReactiveProperty<int> _countProp = new ReactiveProperty<int>(0); // パリィ判定用のカウンター
         
         /// <summary>
         /// ステートに入るときの処理
         /// </summary>
         public override async UniTask Enter()
         {
+            ActionHandler.GuardStart(); // ガード処理を開始
+            
             BlackBoard.ApplyGravity = true;
-            ActionHandler.GuardStart();
-            _count = 0;
             BlackBoard.ParryReception = true; // パリィ受付開始
+            
+            _countProp.Value = 0; // countPropを初期化
+            
+            // countProp の変更を監視
+            _countProp
+                .DistinctUntilChanged() // 値が変更されたときのみ処理を実行
+                .Where(count => count >= BlackBoard.Status.ParryReceptionTime) // パリィ受付終了時間に達したらやめる
+                .Take(1) // 1回だけ処理を実行
+                .Subscribe(count =>
+                {
+                    BlackBoard.ParryReception = false; // パリィ受付終了
+                    Debug.Log("パリィ受付終了");
+                });
             
             await UniTask.Yield();
         }
@@ -32,15 +46,11 @@ namespace PlayerSystem.State.Base
         {
             while (StateMachine.CurrentState.Value == BaseStateEnum.Guard)
             {
-                _count++;
-                if (_count >= BlackBoard.Status.ParryReceptionTime)
-                {
-                    BlackBoard.ParryReception = false; // パリィ受付終了
-                    Debug.Log("パリィ受付終了");
-                }
-
+                _countProp.Value++;
+               
                 ActionHandler.Guard();
 
+                // パリィ受付時間中に攻撃を食らったら、Parry状態に以降
                 if (BlackBoard.ParryReception && BlackBoard.SuccessParry)
                 {
                     BlackBoard.ParryReception = false;
@@ -49,13 +59,15 @@ namespace PlayerSystem.State.Base
                     return;
                 }
                 
+                // ガードブレイク状態
                 if (BlackBoard.IsGuardBreak)
                 {
                     StateMachine.ChangeState(BaseStateEnum.GuardBreak);
                     return;
                 }
 
-                if (InputProcessor.InputBuffer.GetBufferedInput(InputNameEnum.Guard)) // ガードをやめる
+                // ガードをやめる
+                if (InputProcessor.InputBuffer.GetBufferedInput(InputNameEnum.Guard)) 
                 {
                     // 移動入力がなければ Idle へ
                     if (BlackBoard.MoveDirection.sqrMagnitude < 0.01f)
@@ -78,8 +90,10 @@ namespace PlayerSystem.State.Base
         /// </summary>
         public override async UniTask Exit()
         {
+            ActionHandler.GuardEnd(); // ガードをやめる処理を呼ぶ
+            
             BlackBoard.ApplyGravity = false;
-            ActionHandler.GuardEnd();
+            BlackBoard.ParryReception = false;
             
             await UniTask.Yield();
         }
