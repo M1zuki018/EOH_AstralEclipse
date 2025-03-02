97f0ac584a1d673f2d99933abe37716c02326c38
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-02 21:52:59 +0900

    PlayerControllerリファクタリング

--- Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerController.cs
+++ Assets/Scripts/Character/PlayerSystem/Movement/Movement/PlayerController.cs
@@ -3,27 +3,26 @@ using PlayerSystem.ActionFunction;
 using PlayerSystem.Animation;
 using PlayerSystem.Fight;
 using PlayerSystem.Movement;
+using PlayerSystem.State;
 
 /// <summary>
 /// プレイヤーの移動・空中移動などの処理を行う機能
+/// ※PlayerBrainで黒板が生成されるまで動かない
 /// </summary>
 public class PlayerController : MonoBehaviour, IMatchTarget
 { 
     [Header("コンポーネント")]
-    [SerializeField][ReadOnlyOnRuntime] private Transform _playerTransform; // プレイヤーのTransform
     [SerializeField][ReadOnlyOnRuntime] private Transform _playerCamera; // カメラ
     [SerializeField][ReadOnlyOnRuntime] private CharacterController _cc;
-    [SerializeField][ReadOnlyOnRuntime] private PlayerBrain _brain;
     [SerializeField][ReadOnlyOnRuntime] private Animator _animator;
     
-    private FootprintEffect _footprintEffect;
-    
-    public PlayerAnimationController AnimationController => _animController;
-    
-    private Collider _collider;
     [SerializeField] private Transform _targetTransform;
     
+    private Collider _collider;
+    
     #region 各種機能
+    private PlayerBlackBoard _bb;
+    
     private IMovable _mover; //移動
     private IJumpable _jumper; //ジャンプ
     private ISpeedSwitchable _speedSwitcher; //歩きと走り状態の切り替え
@@ -35,6 +34,7 @@ public class PlayerController : MonoBehaviour, IMatchTarget
     private IHandleGroundedCheck _handleGrounded; // 地面にいるときの処理を行うクラス
     private PlayerAnimationController _animController; // アニメーションを制御するクラス
     private GroundTrigger _groundTrigger; // 接地判定を管理するクラス
+    private FootprintEffect _footprintEffect; // 足跡エフェクトを管理するクラス
     
     private PlayerActionHandler _playerActionHandler; // ステートマシンと処理をつなぐ
     public PlayerActionHandler PlayerActionHandler => _playerActionHandler;
@@ -42,51 +42,58 @@ public class PlayerController : MonoBehaviour, IMatchTarget
     
     private void Awake()
     {
+        InitializeComponents(); // 各コンポ―ネントを初期化
+        SetupAnimator(); // アニメーションの設定を行う
+
+        _speedSwitcher.Walk(); // 移動速度切り替えのObservableを購読する
+    }
+
+    #region Awakeの中の処理
+    private void InitializeComponents()
+    {
+        _bb = GetComponent<PlayerBrain>().BB;
+        
+        _combat = GetComponent<PlayerCombat>();
         _footprintEffect = GetComponent<FootprintEffect>();
-        
-        
-        InitializeComponents();
-        
         TryGetComponent(out _collider);
-        _animator.keepAnimatorStateOnDisable = true;
         
+        // インスタンスを生成
+        _animController = new PlayerAnimationController(_bb, _animator);
+        _mover = new PlayerMovementFunction(_bb, _animController, _movementHelper);
+        _jumper = new PlayerJumpFunction(_bb, _cc, _animController,_movementHelper);
+        _speedSwitcher = new PlayerSpeedSwitchFunction(_bb);
+        _movementHelper = new MovementHelper(_playerCamera, _bb, _cc);
+        _groundTrigger = new GroundTrigger(_bb, transform);
+        _playerGravity = new PlayerGravity(_bb, _cc);
+        _handleGrounded = (IHandleGroundedCheck) new HandleGrounded(_bb, _animController);
+        
+        _playerActionHandler = new PlayerActionHandler(
+            mover: _mover,
+            jumper: _jumper,
+            steppable: new StepFunction(_animController, _bb),
+            gauder: new GuardFunction(_bb),
+            attack: (IAttack) _combat,
+            skill: new SkillFunction(_bb),
+            counter: new CounterFunction(_bb));
+        
+        _bb.AnimController = _animController;
+    }
+    
+    /// <summary>
+    /// Animatorの設定を行う
+    /// </summary>
+    private void SetupAnimator()
+    {
+        _animator.keepAnimatorStateOnDisable = true;
+        _animator.applyRootMotion = true; //ルートモーションを有効化
+
         // ターゲットマッチングを行うStateMachineBehaviorに自身を登録する
         foreach (var smb in _animator.GetBehaviours<MatchPositionSMB>())
         {
             smb._target = this;
         }
-        
-        _speedSwitcher.Walk(); // 移動速度切り替えのObservableを購読する
     }
-    
-    private void InitializeComponents()
-    {
-        _movementHelper = new MovementHelper(_playerCamera, _brain.BB, _cc);
-        _animController = new PlayerAnimationController(_brain.BB, _animator);
-        _groundTrigger = new GroundTrigger(_brain.BB, transform);
-        
-        _combat = GetComponent<PlayerCombat>();
-        
-        // インスタンスを生成
-        _mover = new PlayerMovementFunction(_brain.BB, _animController, _movementHelper);
-        _jumper = new PlayerJumpFunction(_brain.BB, _cc, _animController,_movementHelper);
-        _speedSwitcher = new PlayerSpeedSwitchFunction(_brain.BB);
-
-        _playerActionHandler = new PlayerActionHandler(
-            mover: _mover,
-            jumper: _jumper,
-            steppable: new StepFunction(_animController, _brain.BB),
-            gauder: new GuardFunction(_brain.BB),
-            attack: (IAttack) _combat,
-            skill: new SkillFunction(_brain.BB),
-            counter: new CounterFunction(_brain.BB));
-
-        _playerGravity = new PlayerGravity(_brain.BB, _cc);
-        _handleGrounded = (IHandleGroundedCheck) new HandleGrounded(_brain.BB, _animController);
-        
-        _brain.BB.AnimController = _animController;
-        _animator.applyRootMotion = true; //ルートモーションを有効化
-    }
+    #endregion
 
     private void OnDestroy()
     {
@@ -98,17 +105,18 @@ public class PlayerController : MonoBehaviour, IMatchTarget
         _playerGravity.ApplyGravity();
         _groundTrigger.CheckGrounded(); // 接地判定
         
-        if (_brain.BB.IsJumping)
+        if (_bb.IsJumping)
         {
             _jumper.Jumping(); //ジャンプ処理
-            HandleFalling(); //落下中の判定
+            //落下中の判定
         }
         else
         {
             _mover.Move(); //移動処理
-            HandleFalling(); //落下中の判定
+            //落下中の判定
         }
-        
+
+        HandleFalling(); //落下中の判定
         _handleGrounded.HandleGroundedCheck();
     }
 
@@ -117,7 +125,7 @@ public class PlayerController : MonoBehaviour, IMatchTarget
     /// </summary>
     private void HandleFalling()
     {
-        _animController.Movement.PlayFallingAnimation(_brain.BB.IsGrounded);
+        _animController.Movement.PlayFallingAnimation(_bb.IsGrounded);
     }
 
     public Vector3 TargetPosition => _collider.ClosestPoint(_targetTransform.position);
