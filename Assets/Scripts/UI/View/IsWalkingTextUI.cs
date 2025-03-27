813fe3430e2cb9f7c4b942477829478d4287339a
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-27 18:17:19 +0900

    歩きと走りを切り替えた時にわかりやすくする

---
+++ Assets/Scripts/UI/View/IsWalkingTextUI.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using PlayerSystem.State;
+using UI.Interface;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// 歩行中/走行中の切り替えをわかりやすくするUI
+/// </summary>
+[RequireComponent(typeof(CanvasGroup))]
+public class IsWalkingTextUI : MonoBehaviour
+{
+    [SerializeField] private PlayerBrain _playerBrain;
+    private CanvasGroup _canvasGroup;
+    private IDisposable _subscription;
+    
+    private void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        var blackboard = _playerBrain.BB;
+        _subscription = blackboard.IsWalking.Subscribe(_ => Show().Forget()); // 移動速度が変更されたら表示
+        _canvasGroup.alpha = 0;
+    }
+    private async UniTask Show()
+    {
+        _canvasGroup.DOFade(1, 0.3f).SetEase(Ease.OutQuad);
+        _canvasGroup.transform.localScale = Vector3.one;
+        
+        await UniTask.Delay(TimeSpan.FromSeconds(1f));
+        
+        Hide();
+    }
+
+    public void Hide()
+    {
+        _canvasGroup.DOFade(0, 0.3f).SetEase(Ease.OutQuad);
+        _canvasGroup.transform.DOScale(0.95f, 2f).SetEase(Ease.OutQuad);
+    }
+
+    private void OnDestroy()
+    {
+        _subscription?.Dispose();
+    }
+}
